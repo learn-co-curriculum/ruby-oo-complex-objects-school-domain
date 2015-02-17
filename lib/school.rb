99c7bc2066f6c660ef713e16730031a6a1b86a18
Rose Weixel <rose.weixel@gmail.com>
2015-02-17 12:08:18 -0500

    add comments to clarify #sort method

--- lib/school.rb
+++ lib/school.rb
@@ -15,6 +15,7 @@ class School
     roster[student_grade]
   end
 
+  # this method should arrange the students in each grade by alphabetical order
   def sort
     sorted = {}
     roster.each do |grade, students|
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -47,13 +47,14 @@ describe 'School' do
 
   describe "#sort" do
     it 'is able to sort the students' do
+      @school.add_student("Jack Bauer", 7)
       @school.add_student("Homer Simpson", 9)
       @school.add_student("Bart Simpson", 9)
       @school.add_student("Avi Flombaum", 10)
       @school.add_student("Jeff Baird", 10)
       @school.add_student("Blake Johnson", 7)
-      @school.add_student("Jack Bauer", 7)
 
+      # key order does not matter 
       expect(@school.sort).to eq({7 => ["Blake Johnson", "Jack Bauer"], 9 => ["Bart Simpson", "Homer Simpson"], 10 => ["Avi Flombaum", "Jeff Baird"]})
     end
   end
