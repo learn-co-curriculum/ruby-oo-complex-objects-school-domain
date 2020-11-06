684879ca29f39e7fe8242b9357c5342293d3e164
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-11-06 00:03:13 -0600

    commit tree 3f977ef4ce06c62347fe48d83a38a954a0f6c5ce

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School 
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name=name
+        @roster = {}
+    end
+
+    def add_student(student, level)
+        roster[level] ||= []
+        roster[level] << student
+    end
+
+    def grade(level)
+        roster.detect do |x, y| 
+          if x == level
+            return y 
+          end 
+        end 
+    end 
+
+    def sort 
+        nu_hash = {}
+        roster.each do |x, y| 
+          nu_hash[x] = y.sort 
+        end 
+        nu_hash
+    end 
+
+end
