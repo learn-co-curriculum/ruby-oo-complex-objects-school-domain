1cda139c68c99cc719a5b8d8e812e1b391e4db13
Carlos E. La Torre <amautacelt@gmail.com>
2021-02-23 19:09:23 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,28 +2,69 @@
 
 class School
 
-    attr_reader :roster
+    attr_accessor :name, :roster
 
     def initialize(school_name)
-        @school = school_name
+        @school_name =school_name
         @roster = {}
     end
 
     def add_student(name, grade)
-        roster[grade] ||= []
-        roster[grade] << name
+        @roster[grade] ||= []
+        @roster[grade] << name
     end
 
-    def grade(grade)
-        roster[grade]
+    def grade(grade_number)
+        @roster[grade_number]
     end
 
     def sort
-        sorted = {}
-        roster.each do |grade, names|
-            sorted[grade] = names.sort
+        @roster.each do |grade, students|
+            students.sort!
         end
-        sorted
     end
 
 end
+
+
+
+
+
+# def add_student(name, grade)
+#     if !@roster[grade]
+#         @roster[grade] = []
+#         @roster[grade] << name
+#     else
+#     @roster[grade] << name
+#     end
+# end
+
+
+
+# class School
+
+#     attr_reader :roster
+
+#     def initialize(school_name)
+#         @school = school_name
+#         @roster = {}
+#     end
+
+#     def add_student(name, grade)
+#         roster[grade] ||= []
+#         roster[grade] << name
+#     end
+
+#     def grade(grade)
+#         roster[grade]
+#     end
+
+#     def sort
+#         sorted = {}
+#         roster.each do |grade, names|
+#             sorted[grade] = names.sort
+#         end
+#         sorted
+#     end
+
+# end
